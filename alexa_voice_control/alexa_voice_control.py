bc06dcaca437a3f130d55cf0906768fbba67345a
donaldsa18 <donaldsa18@up.edu>
2018-02-09 17:40:47 -0800

    Fixed typos

--- alexa_voice_control/alexa_voice_control.py
+++ alexa_voice_control/alexa_voice_control.py
@@ -60,7 +60,7 @@ class Alexa:
         if data['type'] == 'forward':
             if 'distance' in data and 'distanceUnit' in data:
                 goal.target_pose.pose.position.x = float(data['distance'])
-                if data['angleUnit'] == 'feet':
+                if data['distanceUnit'] == 'feet':
                     goal.target_pose.pose.position.x *= self.FEET_TO_M
             else:
                 goal.target_pose.pose.position.x = 100000.0
@@ -70,13 +70,13 @@ class Alexa:
             angle = float(data['angle'])
             if data['angleUnit'] == 'degrees':
                 angle *= self.DEGREES_TO_RAD
-            goal_quat = quaternion_from_euler(0, 0, angle)
+            goal_quat = quaternion_from_euler(0., 0., angle)
             goal.target_pose.pose.orientation = Quaternion(goal_quat[0], goal_quat[1], goal_quat[2], goal_quat[3])
 
         # Stop the wheelchair
         elif data['type'] == 'stop':
-            goal.target_pose.pose.position = Point(0, 0, 0)
-            goal.target_pose.pose.orientation.x = Quaternion(0, 0, 0, 1)
+            goal.target_pose.pose.position = Point(0., 0., 0.)
+            goal.target_pose.pose.orientation.x = Quaternion(0., 0., 0., 1.)
 
         # Go to the localino tag
         elif data['type'] == 'locateme' and self.tf.frameExists("/base_link") and self.tf.frameExists(
@@ -86,7 +86,7 @@ class Alexa:
 
             # Go to the target, don't care about rotation
             goal.target_pose.pose.position = pos
-            goal.target_pose.pose.orientation = Quaternion(0, 0, 0, 1)
+            goal.target_pose.pose.orientation = Quaternion(0., 0., 0., 1.)
 
         # Go to the static landmark
         elif data['type'] == 'moveto' and self.tf.frameExists("/map") and self.tf.frameExists(
@@ -96,7 +96,7 @@ class Alexa:
 
             # Go to the target, don't care about rotation
             goal.target_pose.pose.position = pos
-            goal.target_pose.pose.orientation = Quaternion(0, 0, 0, 1)
+            goal.target_pose.pose.orientation = Quaternion(0., 0., 0., 1.)
         else:
             rospy.logerr("Could not find transform from base_link to map")
 
