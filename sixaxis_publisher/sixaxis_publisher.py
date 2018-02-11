47c5c909779e4cea2f8c11389fa42dfe37cd5ac7
Anthony Donaldson <donaldsa18@up.edu>
2018-02-10 19:52:34 -0800

    Added error handling to sixaxis publisher

--- sixaxis_publisher/sixaxis_publisher.py
+++ sixaxis_publisher/sixaxis_publisher.py
@@ -66,93 +66,99 @@ class SixaxisPublisher:
         return self.scale(value, (0, 255), (-1, 1))
 
     def begin(self):
-        while not rospy.is_shutdown():
-            event = self.gamepad.read_one()
-            if event is not None:
-                new_x = 0
-                new_y = 0
-                new_rot = 0
-                stop = False
-                rospy.loginfo("Received joystick event: "+str(event))
-                # TODO: optionally publish to cmd_vel topic instead and bypass move_base
-                # Event key mapping can be found here http://www.ev3dev.org/docs/tutorials/using-ps3-sixaxis/
-                # Analog stick moved
-                if event.type == 3:
-                    if event.code == 5:
-                        # moving forward
-                        new_x = 0.1
-                    elif event.code == 0:
-                        # turning
-                        new_rot = self.scale_stick(event.value)*math.pi/32
-                # Key presses
-                elif event.type == 1:
-                    if event.code == 293 and event.value == 0:
-                        # turn right
-                        new_rot = -math.pi / 32
-                    elif event.code == 292 and event.value == 0:
-                        # move forward
-                        new_x = 0.1
-                    elif event.code == 294 and event.value == 0:
-                        # turn right
-                        new_rot = -math.pi / 32
-                    elif event.code == 295 and event.value == 0:
-                        # turn left
-                        new_rot = math.pi / 32
-                    if event.code == 302 and event.value == 1:
-                        # stop
-                        stop = True
-                # Construct message if valid command was read
-                if new_x != 0 or new_y != 0 or new_rot != 0 or stop:
-                    goal = MoveBaseGoal()
-                    goal.target_pose.header.frame_id = "base_link"
-                    goal.target_pose.header.stamp = rospy.get_time()
-                    goal.target_pose.pose.position = Point(new_x, new_y, 0)
-                    quat = quaternion_from_euler(0, 0, new_rot)
-                    goal.target_pose.pose.orientation = Quaternion(quat[0], quat[1], quat[2], quat[3])
-                    self.action_client.send_goal(goal)
-            self.rate.sleep()
+        if hasattr(self, 'gamepad'):
+            while not rospy.is_shutdown():
+                event = self.gamepad.read_one()
+                if event is not None:
+                    new_x = 0
+                    new_y = 0
+                    new_rot = 0
+                    stop = False
+                    rospy.loginfo("Received joystick event: "+str(event))
+                    # TODO: optionally publish to cmd_vel topic instead and bypass move_base
+                    # Event key mapping can be found here http://www.ev3dev.org/docs/tutorials/using-ps3-sixaxis/
+                    # Analog stick moved
+                    if event.type == 3:
+                        if event.code == 5:
+                            # moving forward
+                            new_x = 0.1
+                        elif event.code == 0:
+                            # turning
+                            new_rot = self.scale_stick(event.value)*math.pi/32
+                    # Key presses
+                    elif event.type == 1:
+                        if event.code == 293 and event.value == 0:
+                            # turn right
+                            new_rot = -math.pi / 32
+                        elif event.code == 292 and event.value == 0:
+                            # move forward
+                            new_x = 0.1
+                        elif event.code == 294 and event.value == 0:
+                            # turn right
+                            new_rot = -math.pi / 32
+                        elif event.code == 295 and event.value == 0:
+                            # turn left
+                            new_rot = math.pi / 32
+                        if event.code == 302 and event.value == 1:
+                            # stop
+                            stop = True
+                    # Construct message if valid command was read
+                    if new_x != 0 or new_y != 0 or new_rot != 0 or stop:
+                        goal = MoveBaseGoal()
+                        goal.target_pose.header.frame_id = "base_link"
+                        goal.target_pose.header.stamp = rospy.get_time()
+                        goal.target_pose.pose.position = Point(new_x, new_y, 0)
+                        quat = quaternion_from_euler(0, 0, new_rot)
+                        goal.target_pose.pose.orientation = Quaternion(quat[0], quat[1], quat[2], quat[3])
+                        self.action_client.send_goal(goal)
+                self.rate.sleep()
+        else:
+            rospy.logfatal("Exiting...")
 
     def begin_direct_control(self):
-        while not rospy.is_shutdown():
-            event = self.gamepad.read_one()
-            if event is not None:
-                x_vel = 0
-                rot_vel = 0
-                stop = False
-                rospy.loginfo("Received joystick event: "+(event))
-                # TODO: test if this works or only goes in one direction
-                # Analog stick moved
-                if event.type == 3:
-                    if event.code == 5:
-                        # moving forward
-                        x_vel = self.scale_stick(event.value) * self.MAX_SPEED
-                    elif event.code == 0:
-                        # turning
-                        rot_vel = self.scale_stick(event.value) * self.MAX_ROT_SPEED
-                # Key presses
-                elif event.type == 1:
-                    if event.code == 293 and event.value == 0:
-                        # turn right
-                        rot_vel = -self.MAX_ROT_SPEED / 2
-                    elif event.code == 292 and event.value == 0:
-                        # move forward
-                        x_vel = self.MAX_SPEED / 2
-                    elif event.code == 294 and event.value == 0:
-                        # turn right
-                        rot_vel = -self.MAX_ROT_SPEED / 2
-                    elif event.code == 295 and event.value == 0:
-                        # turn left
-                        rot_vel = self.MAX_ROT_SPEED / 2
-                    elif event.code == 302 and event.value == 1:
-                        # stop
-                        stop = True
-                # Construct message if valid command was read
-                if x_vel != 0 or rot_vel != 0 or stop:
-                    twist = Twist()
-                    twist.linear = Vector3(x_vel, 0, 0)
-                    twist.angular = Vector3(0, 0, rot_vel)
-                    self.pub.publish(twist)
-            self.rate.sleep()
+        if hasattr(self, 'gamepad'):
+            while not rospy.is_shutdown():
+                event = self.gamepad.read_one()
+                if event is not None:
+                    x_vel = 0
+                    rot_vel = 0
+                    stop = False
+                    rospy.loginfo("Received joystick event: "+str(event))
+                    # TODO: test if this works or only goes in one direction
+                    # Analog stick moved
+                    if event.type == 3:
+                        if event.code == 5:
+                            # moving forward
+                            x_vel = self.scale_stick(event.value) * self.MAX_SPEED
+                        elif event.code == 0:
+                            # turning
+                            rot_vel = self.scale_stick(event.value) * self.MAX_ROT_SPEED
+                    # Key presses
+                    elif event.type == 1:
+                        if event.code == 293 and event.value == 0:
+                            # turn right
+                            rot_vel = -self.MAX_ROT_SPEED / 2
+                        elif event.code == 292 and event.value == 0:
+                            # move forward
+                            x_vel = self.MAX_SPEED / 2
+                        elif event.code == 294 and event.value == 0:
+                            # turn right
+                            rot_vel = -self.MAX_ROT_SPEED / 2
+                        elif event.code == 295 and event.value == 0:
+                            # turn left
+                            rot_vel = self.MAX_ROT_SPEED / 2
+                        elif event.code == 302 and event.value == 1:
+                            # stop
+                            stop = True
+                    # Construct message if valid command was read
+                    if x_vel != 0 or rot_vel != 0 or stop:
+                        twist = Twist()
+                        twist.linear = Vector3(x_vel, 0, 0)
+                        twist.angular = Vector3(0, 0, rot_vel)
+                        self.pub.publish(twist)
+                self.rate.sleep()
+        else:
+            rospy.logfatal("Exiting...")
 
 if __name__ == "__main__":
     try:
