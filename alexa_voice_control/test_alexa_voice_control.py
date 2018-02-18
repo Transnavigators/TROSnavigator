dbd0cd7e35fc16252389bf34f1b4e9634acce6fb
Matthew Yuen <yuenm18@up.edu>
2018-02-18 00:28:53 -0800

    Undid changes

--- alexa_voice_control/test_alexa_voice_control.py
+++ alexa_voice_control/test_alexa_voice_control.py
@@ -18,11 +18,12 @@ class TestAlexaVoiceControl(unittest.TestCase):
     def test_one_equals_one(self):
         self.assertEquals(1, 1, "1!=1")
 
-    def test_move_forward(self):
-        rospy.init_node('test_alexa_forward', anonymous=True)
-        rospy.Subscriber("/cmd_vel/goal", MoveBaseAction, self.callback_forward)
-
+    def test_alexa(self):
         self.done = False
+        # set up aws iot
+        # sub = rospy.Subscriber("/cmd_vel/goal", MoveBaseAction, self.callback)
+        self.action_server = actionlib.SimpleActionServer('move_base', MoveBaseAction, self.callback)
+        # start client
 
 
         rospy.sleep(3)
@@ -34,7 +35,8 @@ class TestAlexaVoiceControl(unittest.TestCase):
         rospy.sleep(3)
         self.assertTrue(self.done)
         
-    def callback_forward(self, goal):
+        
+    def callback(self, goal):
         rospy.loginfo("In callback")
         self.assertEqual(goal.target_pose.pose.position.x, 100000.0,"move_forward pose.x")
         self.assertEqual(goal.target_pose.pose.position.y, 0.0,"move_forward pose.y")
@@ -44,35 +46,13 @@ class TestAlexaVoiceControl(unittest.TestCase):
         self.assertEqual(goal.target_pose.pose.orientation.z, 0.0,"move_forward orientation.z")
         self.assertEqual(goal.target_pose.pose.orientation.w, 0.0,"move_forward orientation.w")
         self.done = True
+        self.action_server.set_succeeded()
 
 
-    def test_stop(self):
-        rospy.init_node('test_alexa_stop', anonymous=True)
-        rospy.Subscriber("/cmd_vel/goal", MoveBaseAction, self.callback_stop)
-        self.done = False
-        # set up aws iot
-       
-        # test forward
-        message = json.dumps({"type": "stop"})
-        aws_iot_mqtt_client.publish(topic, message, 1)
-
-        rospy.sleep(3)
-        self.assertTrue(self.done)
-        
-    def callback_stop(self, goal):
-        rospy.loginfo("In callback")
-        self.assertEqual(goal.target_pose.pose.position.x, 0.0,"stop pose.x")
-        self.assertEqual(goal.target_pose.pose.position.y, 0.0,"stop pose.y")
-        self.assertEqual(goal.target_pose.pose.position.z, 0.0,"stop pose.z")
-        self.assertEqual(goal.target_pose.pose.orientation.x, 0.0,"stop orientation.x")
-        self.assertEqual(goal.target_pose.pose.orientation.y, 0.0,"stop orientation.y")
-        self.assertEqual(goal.target_pose.pose.orientation.z, 0.0,"stop orientation.z")
-        self.assertEqual(goal.target_pose.pose.orientation.w, 0.0,"stop orientation.w")
-        self.done = True
-        
 if __name__ == '__main__':
     import rostest
 
+    rospy.init_node('test_alexa', anonymous=True)
     # set up AWS constants
     if rospy.has_param("~host"):
         host = rospy.get_param("~host")
@@ -98,7 +78,7 @@ if __name__ == '__main__':
         topic = rospy.get_param("~topic")
     else:
         topic = '/Transnavigators/Pi'
-    # start client
+    
     aws_iot_mqtt_client = AWSIoTMQTTClient(clientId)
     aws_iot_mqtt_client.configureEndpoint(host, 8883)
     aws_iot_mqtt_client.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
@@ -109,7 +89,7 @@ if __name__ == '__main__':
     aws_iot_mqtt_client.configureDrainingFrequency(2)  # Draining: 2 Hz
     aws_iot_mqtt_client.configureConnectDisconnectTimeout(10)  # 10 sec
     aws_iot_mqtt_client.configureMQTTOperationTimeout(5)  # 5 sec
-
+    
     # Connect to AWS IoT
     aws_iot_mqtt_client.connect()
     rospy.loginfo("Connecting to AWS")
