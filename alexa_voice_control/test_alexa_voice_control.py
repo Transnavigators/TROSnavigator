7f12c0a33408c6fb06a4d1c40862387c38207c8d
Matthew Yuen <yuenm18@up.edu>
2018-02-17 22:06:36 -0800

    Moved set up to outside rostest

--- alexa_voice_control/test_alexa_voice_control.py
+++ alexa_voice_control/test_alexa_voice_control.py
@@ -6,6 +6,7 @@ from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 import logging
 import json
 import os
+import time
 from move_base_msgs.msg import MoveBaseActionGoal    
 
 package_name = 'alexa_voice_control'
@@ -19,62 +20,6 @@ class TestAlexaVoiceControl(unittest.TestCase):
         self.assertEquals(1, 1, "1!=1")
 
     def test_alexa(self):
-        rospy.init_node('test_alexa', anonymous=True)
-
-        # set up AWS constants
-        if rospy.has_param("~host"):
-            self.host = rospy.get_param("host")
-        else:
-            self.host = 'a1vgqh9vgvjzyh.iot.us-east-1.amazonaws.com'
-        if rospy.has_param("~rootCAPath"):
-            self.rootCAPath = os.path.dirname(os.path.abspath(__file__)) + rospy.get_param("~rootCAPath")
-        else:
-            self.rootCAPath = os.path.dirname(os.path.abspath(__file__)) + '/Certificates/root-CA.crt'
-        if rospy.has_param("~certificatePath"):
-            self.certificatePath = os.path.dirname(os.path.abspath(__file__)) + rospy.get_param("~certificatePath")
-        else:
-            self.certificatePath = os.path.dirname(os.path.abspath(__file__)) + '/Certificates/Pi.cert.pem'
-        if rospy.has_param("~privateKeyPath"):
-            self.privateKeyPath = os.path.dirname(os.path.abspath(__file__)) + rospy.get_param("~privateKeyPath")
-        else:
-            self.privateKeyPath = os.path.dirname(os.path.abspath(__file__)) + '/Certificates/Pi.private.key'
-        if rospy.has_param("~clientId"):
-            self.clientId = rospy.get_param("~clientId")
-        else:
-            self.clientId = 'test_pi'
-        if rospy.has_param("~topic"):
-            self.topic = rospy.get_param("~topic")
-        else:
-            self.topic = '/Transnavigators/Pi'
-
-        
-        
-        # set up aws iot
-        rospy.loginfo("Connecting to AWS")
-    
-        # Configure logging
-        logger = logging.getLogger("AWSIoTPythonSDK.core")
-        logger.setLevel(logging.WARN)
-        stream_handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        stream_handler.setFormatter(formatter)
-        logger.addHandler(stream_handler)
-
-        self.aws_iot_mqtt_client = AWSIoTMQTTClient(self.clientId)
-        self.aws_iot_mqtt_client.configureEndpoint(self.host, 8883)
-        self.aws_iot_mqtt_client.configureCredentials(self.rootCAPath, self.privateKeyPath, self.certificatePath)
-
-        # AWSIoTMQTTClient connection configuration
-        self.aws_iot_mqtt_client.configureAutoReconnectBackoffTime(1, 32, 20)
-        self.aws_iot_mqtt_client.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-        self.aws_iot_mqtt_client.configureDrainingFrequency(2)  # Draining: 2 Hz
-        self.aws_iot_mqtt_client.configureConnectDisconnectTimeout(10)  # 10 sec
-        self.aws_iot_mqtt_client.configureMQTTOperationTimeout(5)  # 5 sec
-
-        # Connect to AWS IoT
-        self.aws_iot_mqtt_client.connect()
-        
-        
         
         # test forward
         self.result = False
@@ -82,44 +27,98 @@ class TestAlexaVoiceControl(unittest.TestCase):
         message = json.dumps({"type" : "forward"})
         self.aws_iot_mqtt_client.publish(self.topic, message, 1)
         while self.result == False:
+            rospy.loginfo("Waiting for info 1")
             time.sleep(1)
         sub.unregister()
         
         
         # test stop
-        while self.test != 2:
-            pass
         self.result = False
         sub = rospy.Subscriber("/cmd_vel/goal", MoveBaseActionGoal, self.stop_callback)
         message = json.dumps({"type" : "stop"})
         self.aws_iot_mqtt_client.publish(self.topic, message, 1)
         while self.result == False:
+            rospy.loginfo("Waiting for info 2")
             time.sleep(1)
         sub.unregister()
         
     def move_forward_callback(self, msg):
         
-        assertEqual(msg.goal.pose.x, 100000.0,"move_forward pose.x")
-        assertEqual(msg.goal.pose.y, 0.0,"move_forward pose.y")
-        assertEqual(msg.goal.pose.z, 0.0,"move_forward pose.z")
-        assertEqual(msg.goal.orientation.x, 0.0,"move_forward orientation.x")
-        assertEqual(msg.goal.orientation.y, 0.0,"move_forward orientation.y")
-        assertEqual(msg.goal.orientation.z, 0.0,"move_forward orientation.z")
-        assertEqual(msg.goal.orientation.w, 0.0,"move_forward orientation.w")
+        self.assertEqual(msg.goal.pose.x, 100000.0,"move_forward pose.x")
+        self.assertEqual(msg.goal.pose.y, 0.0,"move_forward pose.y")
+        self.assertEqual(msg.goal.pose.z, 0.0,"move_forward pose.z")
+        self.assertEqual(msg.goal.orientation.x, 0.0,"move_forward orientation.x")
+        self.assertEqual(msg.goal.orientation.y, 0.0,"move_forward orientation.y")
+        self.assertEqual(msg.goal.orientation.z, 0.0,"move_forward orientation.z")
+        self.assertEqual(msg.goal.orientation.w, 0.0,"move_forward orientation.w")
         self.result = True
 
         
     def stop_callback(self, msg):
         
-        assertEqual(msg.goal.pose.x, 0.0,"stop pose.x")
-        assertEqual(msg.goal.pose.y, 0.0,"stop pose.y")
-        assertEqual(msg.goal.pose.z, 0.0,"stop pose.z")
-        assertEqual(msg.goal.orientation.x, 0.0,"stop orientation.x")
-        assertEqual(msg.goal.orientation.y, 0.0,"stop orientation.y")
-        assertEqual(msg.goal.orientation.z, 0.0,"stop orientation.z")
-        assertEqual(msg.goal.orientation.w, 0.0,"stop orientation.w")
+        self.assertEqual(msg.goal.pose.x, 0.0,"stop pose.x")
+        self.assertEqual(msg.goal.pose.y, 0.0,"stop pose.y")
+        self.assertEqual(msg.goal.pose.z, 0.0,"stop pose.z")
+        self.assertEqual(msg.goal.orientation.x, 0.0,"stop orientation.x")
+        self.assertEqual(msg.goal.orientation.y, 0.0,"stop orientation.y")
+        self.assertEqual(msg.goal.orientation.z, 0.0,"stop orientation.z")
+        self.assertEqual(msg.goal.orientation.w, 0.0,"stop orientation.w")
         self.result = True
         
 if __name__ == '__main__':
     import rostest
+        rospy.init_node('test_alexa', anonymous=True)
+
+    # set up AWS constants
+    if rospy.has_param("~host"):
+        host = rospy.get_param("host")
+    else:
+        host = 'a1vgqh9vgvjzyh.iot.us-east-1.amazonaws.com'
+    if rospy.has_param("~rootCAPath"):
+        rootCAPath = os.path.dirname(os.path.abspath(__file__)) + rospy.get_param("~rootCAPath")
+    else:
+        rootCAPath = os.path.dirname(os.path.abspath(__file__)) + '/Certificates/root-CA.crt'
+    if rospy.has_param("~certificatePath"):
+        certificatePath = os.path.dirname(os.path.abspath(__file__)) + rospy.get_param("~certificatePath")
+    else:
+        certificatePath = os.path.dirname(os.path.abspath(__file__)) + '/Certificates/Pi.cert.pem'
+    if rospy.has_param("~privateKeyPath"):
+        privateKeyPath = os.path.dirname(os.path.abspath(__file__)) + rospy.get_param("~privateKeyPath")
+    else:
+        privateKeyPath = os.path.dirname(os.path.abspath(__file__)) + '/Certificates/Pi.private.key'
+    if rospy.has_param("~clientId"):
+        clientId = rospy.get_param("~clientId")
+    else:
+        clientId = 'test_pi'
+    if rospy.has_param("~topic"):
+        topic = rospy.get_param("~topic")
+    else:
+        topic = '/Transnavigators/Pi'
+
+    
+    
+    # set up aws iot
+    rospy.loginfo("Connecting to AWS")
+    
+    # Configure logging
+    logger = logging.getLogger("AWSIoTPythonSDK.core")
+    logger.setLevel(logging.WARN)
+    stream_handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    aws_iot_mqtt_client = AWSIoTMQTTClient(clientId)
+    aws_iot_mqtt_client.configureEndpoint(host, 8883)
+    aws_iot_mqtt_client.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+    # AWSIoTMQTTClient connection configuration
+    aws_iot_mqtt_client.configureAutoReconnectBackoffTime(1, 32, 20)
+    aws_iot_mqtt_client.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+    aws_iot_mqtt_client.configureDrainingFrequency(2)  # Draining: 2 Hz
+    aws_iot_mqtt_client.configureConnectDisconnectTimeout(10)  # 10 sec
+    aws_iot_mqtt_client.configureMQTTOperationTimeout(5)  # 5 sec
+
+    # Connect to AWS IoT
+    aws_iot_mqtt_client.connect()
     rostest.rosrun(package_name, test_name, TestAlexaVoiceControl)
