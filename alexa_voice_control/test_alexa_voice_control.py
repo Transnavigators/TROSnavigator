98da83729eeb08aff1539855dd8765f4d0163c70
Matthew Yuen <yuenm18@up.edu>
2018-02-18 21:37:00 -0800

    Fixed another typo

--- alexa_voice_control/test_alexa_voice_control.py
+++ alexa_voice_control/test_alexa_voice_control.py
@@ -28,7 +28,7 @@ class TestAlexaVoiceControl(unittest.TestCase):
             (json.dumps({"type": "stop"}), [0.0,0.0,0.0,0.0,0.0,0.0,1.0]), \
             (json.dumps({"type": "turn"}), [0.0,0.0,0.0,0.0,0.0,0.0,0.0], \
             (json.dumps({"type": "forward","distance": 10, "distanceUnit": "meters"}), [0.0,0.0,0.0,0.0,0.0,0.0,0.0], \
-            (json.dumps({"type": "turn",  "angle" : 45, "angleUnit" : "degrees"})[0.0,0.0,0.0,0.0,0.0,0.0,0.0] \
+            (json.dumps({"type": "turn",  "angle" : 45, "angleUnit" : "degrees"}),[0.0,0.0,0.0,0.0,0.0,0.0,0.0] \
         }
         self.current_pose = 0
         
