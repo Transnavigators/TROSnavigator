7b6c19d4657a60b7aa9c93f0a5bc6d6e9d21d1e0
Transnavigators <Transnavigators>
2018-02-27 15:05:03 -0800

    Fixed crashing problem

--- arduino_odometry/arduino_odometry.py
+++ arduino_odometry/arduino_odometry.py
@@ -64,90 +64,94 @@ class ArduinoOdometry:
         
         while not rospy.is_shutdown():
             # read encoders
-            receive_data = self.readEncoders()
-            new_left,new_right = struct.unpack('ii',bytearray(receive_data[0:8]))
-            
-            rospy.loginfo("Left count: ", new_left, " | Right count: ", new_right)
-            
-            current_time = rospy.get_time()
-            now = rospy.Time.now()
+            try:
 
-            # adapted from Mastering ROS for Robotics Programming page 303-306
-            # https://github.com/qboticslabs/mastering_ros/blob/master/chapter_9_codes/chefbot_navig_cpp/src/diff_tf.cpp
-            ##################################################################
-            elapsed = current_time - previous_time
-            if old_left == 0: # reset? we might not need this check 
-                d_left = 0
-                d_right = 0
-            else:
+                receive_data = self.readEncoders()
+                new_left,new_right = struct.unpack('ii',bytearray(receive_data[0:8]))
+            
+                rospy.loginfo("Left count: "+str(new_left)+" | Right count: "+str(new_right))
+            
+                current_time = rospy.get_time()
+                now = rospy.Time.now()
+
+                # adapted from Mastering ROS for Robotics Programming page 303-306
+                # https://github.com/qboticslabs/mastering_ros/blob/master/chapter_9_codes/chefbot_navig_cpp/src/diff_tf.cpp
+                ##################################################################
+                elapsed = current_time - previous_time
+                #if old_left == 0: # reset? we might not need this check 
+                #    d_left = 0
+                #    d_right = 0
+                #else:
                 d_left = (new_left - old_left) * self.meters_per_pulse
                 d_right = (new_right - old_right) * self.meters_per_pulse
 
-            old_left = new_left
-            old_right = new_right
-            previous_time = current_time
+                old_left = new_left
+                old_right = new_right
+                previous_time = current_time
             
-            d = (d_left + d_right ) / 2.0
-            th = ( d_right - d_left ) / self.width
-            dx = d /elapsed
-            dr = th / elapsed
-            if d != 0:
-                x = math.cos( th ) * d
-                y = -math.sin( th ) * d
-                # calculate the final position of the robot
-                x_final = x_final + ( math.cos( theta_final ) * x - math.sin(theta_final ) * y )
-                y_final = y_final + ( math.sin( theta_final ) * x + math.cos(theta_final ) * y )
-            if th != 0:
-                theta_final = theta_final + th
+                d = (d_left + d_right ) / 2.0
+                th = ( d_right - d_left ) / self.width
+                dx = d /elapsed
+                dr = th / elapsed
+                if d != 0:
+                    x = math.cos( th ) * d
+                    y = -math.sin( th ) * d
+                    # calculate the final position of the robot
+                    x_final = x_final + ( math.cos( theta_final ) * x - math.sin(theta_final ) * y )
+                    y_final = y_final + ( math.sin( theta_final ) * x + math.cos(theta_final ) * y )
+                if th != 0:
+                    theta_final = theta_final + th
                 
                 
                 
-            # send messages
-            odom_quat = Quaternion()
-            odom_quat.x = 0.0
-            odom_quat.y = 0.0
-            odom_quat.z = 0.0
+                # send messages
+                odom_quat = Quaternion()
+                odom_quat.x = 0.0
+                odom_quat.y = 0.0
+                odom_quat.z = 0.0
             
-            odom_quat.z = math.sin( theta_final / 2 )
-            odom_quat.w = math.cos( theta_final / 2 )
+                odom_quat.z = math.sin( theta_final / 2 )
+                odom_quat.w = math.cos( theta_final / 2 )
             
-            # # first, we'll publish the transform over tf
-            # odom_trans = TransformStamped()
-            # odom_trans.header.stamp = now
-            # odom_trans.header.frame_id = "odom"
-            # odom_trans.child_frame_id = "base_link"
+                # # first, we'll publish the transform over tf
+                # odom_trans = TransformStamped()
+                # odom_trans.header.stamp = now
+                # odom_trans.header.frame_id = "odom"
+                # odom_trans.child_frame_id = "base_link"
             
-            # odom_trans.transform.translation.x = x_final
-            # odom_trans.transform.translation.y = y_final
-            # odom_trans.transform.translation.z = 0.0
-            # odom_trans.transform.rotation = odom_quat
-            #send the transform
-            self.odom_broadcaster.sendTransform((x_final,y_final, 0.0),
+                # odom_trans.transform.translation.x = x_final
+                # odom_trans.transform.translation.y = y_final
+                # odom_trans.transform.translation.z = 0.0
+                # odom_trans.transform.rotation = odom_quat
+                #send the transform
+                self.odom_broadcaster.sendTransform((x_final,y_final, 0.0),
                                                 tf.transformations.quaternion_from_euler(0,0,theta_final),
                                                 now,
                                                 "base_link",
                                                 "odom")
             
-            #next, we'll publish the odometry message over ROS
-            odom = Odometry()
-            odom.header.stamp = now
-            odom.header.frame_id = "odom"
-            #set the position
-            odom.pose.pose.position.x = x_final
-            odom.pose.pose.position.y = y_final
-            odom.pose.pose.position.z = 0.0
-            odom.pose.pose.orientation = odom_quat
-            #set the velocity
-            odom.child_frame_id = "base_link"
-            odom.twist.twist.linear.x = dx
-            odom.twist.twist.linear.y = 0
-            odom.twist.twist.angular.z = dr
+                #next, we'll publish the odometry message over ROS
+                odom = Odometry()
+                odom.header.stamp = now
+                odom.header.frame_id = "odom"
+                #set the position
+                odom.pose.pose.position.x = x_final
+                odom.pose.pose.position.y = y_final
+                odom.pose.pose.position.z = 0.0
+                odom.pose.pose.orientation = odom_quat
+                #set the velocity
+                odom.child_frame_id = "base_link"
+                odom.twist.twist.linear.x = dx
+                odom.twist.twist.linear.y = 0
+                odom.twist.twist.angular.z = dr
             
-            self.pub.publish(odom)
+                self.pub.publish(odom)
             
-            ###########################################################
-            rate.sleep()
-
+                ###########################################################
+                rate.sleep()
+            except IOError as e:
+                rospy.logwarn(e)
+                pass
 
 if __name__ == "__main__":
     try:
