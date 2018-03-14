b67135ad34b12d89589f855c7740c0c429fcb7a4
Anthony Donaldson <donaldsa18@up.edu>
2018-03-14 16:23:23 -0700

    Wrote documentation for the arduino motor node

--- arduino_motor/arduino_motor.py
+++ arduino_motor/arduino_motor.py
@@ -9,13 +9,18 @@ from geometry_msgs.msg import Twist, TransformStamped, Quaternion
 
 
 class ArduinoMotor:
-    # commands for sending and receiving it
-
+    """Sends commands to the Arduino to relay to the motor controller
+    
+    """
     def __init__(self):
         # Initialize the serial port
         rospy.init_node('arduino_motor', anonymous=True)
         rospy.loginfo("%s started" % rospy.get_name())
 
+        # Get params
+        self.width = float(rospy.get_param("~width", 31.5 * 0.0254))
+        self.rate = int(rospy.get_param("~rate", 20))
+        self.retry_limit = int(rospy.get_param("~retry_limit", 10))
         reset_pin = int(rospy.get_param("reset_pin", 4))
 
         # Setup pin 4 as an output pin for resetting the Arduino
@@ -27,44 +32,52 @@ class ArduinoMotor:
         GPIO.output(reset_pin, GPIO.LOW)
         rospy.sleep(1.0)
         GPIO.output(reset_pin, GPIO.HIGH)
+        rospy.sleep(1.0)
 
-        self.width = float(rospy.get_param("~width", 31.5 * 0.0254))
-
-        self.constant = 45.0
+        # Setup the i2c bus
         self.bus = smbus.SMBus(1)
 
-        self.move_cmd = ord('m')
-        # self.encoder_cmd = ord('e')
+        # The address and command byte to use for i2c communication
         self.address = 0x04
-
-        self.dx = 0
-        self.dr = 0
-        self.dy = 0
+        self.move_cmd = ord('m')
 
         rospy.Subscriber("cmd_vel", Twist, self.callback)
 
-        self.rate = int(rospy.get_param("~rate", 20))
         self.left = 0
         self.right = 0
 
     def callback(self, msg):
-        # rospy.loginfo("twist to motors:: twistCallback raw msg: %s" % str(msg))
-        self.dx = msg.linear.x
-        self.dr = msg.angular.z
-        self.dy = msg.linear.y
-        if self.dx != 0 or self.dr != 0 or self.dy != 0:
-            rospy.loginfo("linear.x = " + str(self.dx) + " angular.z: " + str(self.dr) + " linear.y: " + str(self.dy))
+        """Updates the linear and angular velocity instance variables
+        
+        The message definition can be found here: http://docs.ros.org/api/geometry_msgs/html/msg/Twist.html
+        
+        Args:
+            msg (geometry_msgs.msg.Twist): The desired speed
+
+        """
+        dx = msg.linear.x
+        dr = msg.angular.z
+        self.right = 1.0 * dx + dr * self.width / 2
+        self.left = 1.0 * dx - dr * self.width / 2
+        if dx != 0 or dr != 0:
+            rospy.loginfo("linear.x = " + str(dx) + " angular.z: " + str(dr))
 
     # send data to arduino
     def send_speed_to_motor(self, m1, m2):
+        """
+        
+        Args:
+            m1 (float): The left motor's speed in m/s 
+            m2 (float): The right motor's speed in m/s 
+
+        """
         val = list(bytearray(struct.pack("=ff", m1, m2)))
-        # rospy.logwarn("Sending %d bytes" % len(val))
-        # rospy.logwarn("[%s]" % ", ".join(map(str, val)))
         self.bus.write_i2c_block_data(self.address, self.move_cmd, list(val))
 
-    # start the node: spin forever
     def begin(self):
+        """Start the node and spin forever
 
+        """
         r = rospy.Rate(self.rate)
         # main loop
         while not rospy.is_shutdown():  # and self.ticks_since_target < self.timeout_ticks:
@@ -72,17 +85,17 @@ class ArduinoMotor:
             r.sleep()
 
     def spin_once(self):
-        self.right = 1.0 * self.dx + self.dr * self.width / 2
-        self.left = 1.0 * self.dx - self.dr * self.width / 2
-        # rospy.loginfo("twist to motors:: spinOnce (dx:%f, dr: %f)", self.dx,self.dr)
-        # rospy.loginfo("twist to motors:: spinOnce (self.left:%f,self.right %f)" % (self.left,self.right) )
-        # rospy.loginfo("LEFT: " +str(int(self.left*self.constant))+"RIGHT: " +str(int(self.right*self.constant)))
-        while not rospy.is_shutdown():
+        """Sends the speeds to the motor once and retries if it fails
+
+        """
+        count = 0
+        while not rospy.is_shutdown() and count < self.retry_limit:
             try:
                 self.send_speed_to_motor(float(self.left), float(self.right))
                 rospy.loginfo_throttle(1, "Sent speed successfully")
                 break
             except IOError:
+                count += 1
                 pass
 
 
