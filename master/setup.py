ea7baad01047a66e6bcc03a8bc59c2d87c21672c
Matthew Yuen <yuenm18@up.edu>
2018-03-26 00:46:42 -0700

    Fixed setup

--- master/setup.py
+++ master/setup.py
@@ -4,7 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=[''],
-    scripts=['arduino_motor.py'],
+    scripts=['master.py'],
     package_dir={'': 'src'})
 
 setup(**setup_args)
