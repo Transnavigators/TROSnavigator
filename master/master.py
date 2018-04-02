48aed5e732ffa6ab44173e06739dbbe22721fcfa
Matthew Yuen <yuenm18@up.edu>
2018-04-02 14:41:49 -0700

    Fixed the consistant turns

--- master/master.py
+++ master/master.py
@@ -104,8 +104,12 @@ class Master:
             
             dist = math.sqrt((self.desired_position_x-self.current_position_x)**2+(self.desired_position_y-self.current_position_y)**2)
             
-            # get delta orientation in the range -pi to pi
-            orientation_err = (self.desired_orientation - self.current_orientation)-math.floor((self.desired_orientation - self.current_orientation)/math.pi)*math.pi
+            # get delta orientation in the range -pi to pi so we always take the short way around
+            orientation_err = (self.desired_orientation - self.current_orientation)
+            if orientation_err > math.pi:
+                orientation_err = orientation_err - 2*math.pi
+            elif orientation_err < -math.pi
+                orientation_erro = orientation_err + 2*math.pi
             
 
             # we are trying to move forward
